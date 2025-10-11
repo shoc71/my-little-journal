e1dd79013dda9d9a9d3c5573bba0348a190f13c3
shoc71 <supersonicwind6@gmail.com>
2025-10-11 19:01:26 -0400

    server connection

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import journalRoute from "./routes/journalRoute.js"
 
 // middleware
 dotenv.config();
@@ -15,7 +14,6 @@ app.use(express.json());
 app.use(cors());
 
 // routes (paths)
-app.use("/api/journal", journalRoute);
 
 // server
 app.listen(PORT, () => {
