8c547875c1ac389129a6cbed6d55ff7a556b7b65
shoc71 <supersonicwind6@gmail.com>
2025-10-20 22:45:52 -0400

    userModel

---
+++ server/models/user.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userModel = new mongoose.Schema({
+    username : {
+        type: String,
+        required: [true, "Please add a name"],
+        minlength: [4, "Please make the name at least 4 characters."]
+    },
+    email: {
+        type: String,
+        required: [true, "Please add an email"],
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        minlength: [6, "Password must be at least 6 characters"],
+    },
+    story: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Story"
+    },
+}, {
+    timestamps : true
+});
+
+
+userModel.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(11);
+        this.password = await bcrypt.hash(this.password, hash);
+        next();
+    } catch (error) {
+        console.error("User Password Hashing Error: ", error.message);
+        next(error);
+    }
+});
+
+userModel.methods.matchPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userModel.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", userModel);
